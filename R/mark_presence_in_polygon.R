e5a91ec7df54035d94a8a9d247a0a5a31950fba7
B-Nilson <nilson@unbc.ca>
2025-12-12 11:18:28 -0800

    fix handling of edge cases and cleanup

--- R/mark_presence_in_polygon.R
+++ R/mark_presence_in_polygon.R
@@ -5,13 +5,15 @@ mark_presence_in_polygon <- function(x, y, id_col = "id") {
     sf::st_within(y, sparse = FALSE) |>
     which(arr.ind = TRUE) |>
     as.data.frame() |>
+    dplyr::distinct(row, .keep_all = TRUE) |>
     dplyr::mutate(!!id_col := as.character(y[[id_col]][col])) |>
     tidyr::complete(row = seq(1, max(row))) |>
     dplyr::arrange(row)
   x[[id_col]] <- fully_within_y[[id_col]]
 
   # Mark the ids of the polygons that each cell is partially within
-  not_within <- x[fully_within_y$row[is.na(fully_within_y$col)], ]
+  is_not_within <- fully_within_y$row[is.na(fully_within_y$col)]
+  not_within <- x[is_not_within, ]
   if (nrow(not_within) == 0) {
     return(x)
   }
@@ -19,15 +21,19 @@ mark_presence_in_polygon <- function(x, y, id_col = "id") {
     sf::st_overlaps(y, sparse = FALSE) |>
     which(arr.ind = TRUE) |>
     as.data.frame() |>
-    dplyr::mutate(!!id_col := y[[id_col]][col]) |>
-    # Handle cells bordering multiple shapes
-    dplyr::group_by(row) |>
-    dplyr::summarise(!!id_col := paste(!!rlang::sym(id_col), collapse = ",")) |>
-    tidyr::complete(row = seq(1, max(row))) |>
-    dplyr::arrange(row)
+    dplyr::mutate(!!id_col := y[[id_col]][col])
 
-  x[[id_col]][fully_within_y$row[is.na(fully_within_y$col)]] <- overlaps_y[[
-    id_col
-  ]]
+  if (nrow(overlaps_y) > 0) {
+    overlaps_y <- overlaps_y |>
+      # Handle cells bordering multiple shapes
+      dplyr::group_by(row) |>
+      dplyr::summarise(
+        !!id_col := paste(!!rlang::sym(id_col), collapse = ",")
+      ) |>
+      tidyr::complete(row = seq(1, max(row))) |>
+      dplyr::arrange(row)
+
+    x[[id_col]][is_not_within] <- overlaps_y[[id_col]]
+  }
   return(x)
 }
