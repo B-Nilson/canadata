80a1989f3d640d3c1eabfddaa3c0a09288c39c23
B-Nilson <nilson.brayden@gmail.com>
2025-12-14 22:10:31 -0800

    pass lazy data directly to avoid promise warning - how to fix?

--- tests/testthat/test-view_data.R
+++ tests/testthat/test-view_data.R
@@ -1,12 +1,12 @@
 test_that("no warnings/errors for communities", {
   skip_if_not(interactive())
-  view_communities() |>
+  view_communities(communities) |>
     expect_silent()
 })
 
 test_that("no warnings/errors for indigenous communities", {
   skip_if_not(interactive())
-  view_indigenous_communities() |>
+  view_indigenous_communities(indigenous_communities) |>
     expect_silent()
 })
 
@@ -18,19 +18,19 @@ test_that("no warnings/errors for indigenous lands", {
 
 test_that("no warnings/errors for forecast zones", {
   skip_if_not(interactive())
-  view_forecast_zones() |>
+  view_forecast_zones(forecast_zones) |>
     expect_silent()
 })
 
 test_that("no warnings/errors for provinces and territories", {
   skip_if_not(interactive())
-  view_provinces_and_territories() |>
+  view_provinces_and_territories(provinces_and_territories) |>
     expect_silent()
 })
 
 test_that("no warnings/errors for gridded population", {
   skip_if_not(interactive())
-  view_gridded_2016_population() |>
+  view_gridded_2016_population(gridded_2016_population) |>
     expect_silent()
 })
 
