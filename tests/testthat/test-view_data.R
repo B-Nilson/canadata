33fc1949d22ee72fd5b84ae21d0777cad10c826c
B-Nilson <nilson.brayden@gmail.com>
2025-12-14 20:20:33 -0800

    add test

--- tests/testthat/test-view_data.R
+++ tests/testthat/test-view_data.R
@@ -36,6 +36,20 @@ test_that("no warnings/errors for all layers together", {
       dplyr::filter(is.na(fcst_zones)),
     provinces_and_territories = provinces_and_territories,
     forecast_zones = forecast_zones,
+    communities = communities,
+    indigenous_communities = indigenous_communities
+  ) |>
+    expect_silent()
+})
+
+test_that("all locations not in a forecast zone are expected", {
+  skip_if_not(interactive())
+
+  view_all_layers(
+    gridded_2016_population = gridded_2016_population |>
+      dplyr::filter(is.na(fcst_zones)),
+    provinces_and_territories = provinces_and_territories,
+    forecast_zones = forecast_zones,
     communities = communities |> dplyr::filter(is.na(fcst_zone)),
     indigenous_communities = indigenous_communities |>
       dplyr::filter(is.na(fcst_zone))
